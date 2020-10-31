cc25d2c0bcceb66c83d95f17a64b209ff1eacc6b
Максим Богащенко <pl_a_st@live.ru>
2020-10-31 20:17:23 +0300

    ДОбавление класса Human

---
+++ HomWorkClasses25.10.2020_1/Human.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+public struct ShareLandPlot
+{
+    public int inventoryNumber;
+    public double share;
+}
+
+namespace HomWorkClasses25._10._2020_1
+{
+    class Human
+    {
+        public string Name
+        {
+            get; private set;
+        }
+        public string Surname
+        {
+            get; private set;
+        }
+        public string MiddleName
+        {
+            get; private set;
+        }
+        public int Age
+        {
+            get; private set;
+        }
+        public List<ShareLandPlot> SharesLandPlots
+        {
+            get; private set;
+        }
+        public Human(string name, string surname, string middleName, int age)
+        {
+            Name = name;
+            Surname = surname;
+            MiddleName = middleName;
+            Age = age;
+        }
+        public void 
+    }
+}
