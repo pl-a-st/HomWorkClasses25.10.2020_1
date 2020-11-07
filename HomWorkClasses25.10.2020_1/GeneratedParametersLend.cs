4c810b6703d49c06583fad2bf46574ab6405aa39
Максим Богащенко <pl_a_st@live.ru>
2020-11-07 11:38:23 +0300

    07.11.20 11:38

--- HomWorkClasses25.10.2020_1/GeneratedParametersLend.cs
+++ HomWorkClasses25.10.2020_1/GeneratedParametersLend.cs
@@ -7,30 +7,6 @@ namespace HomWorkClasses25._10._2020_1
 {
    public class GeneratedParametersLend
     {
-
-        /// <summary>
-        /// Генерирует семизначный номер
-        /// </summary>
-        /// <param name="Лист сгенерированных номеров"></param>
-        //public static int GenerateInventoryNumber(List<int> availablePassportNumbers)
-        //{
-        //    const int MAX_INVENTORY_NUMBER = 10000000;
-        //    const int MIN_INVENTORY_NUMBER = 1000000;
-        //    int inventoryNumber;
-        //    Random rnd = new Random();
-        //    do
-        //    {
-        //        inventoryNumber = rnd.Next(MIN_INVENTORY_NUMBER, MAX_INVENTORY_NUMBER);
-        //        if (!availablePassportNumbers.Contains(inventoryNumber))
-        //        {
-        //            availablePassportNumbers.Add(inventoryNumber);
-        //            return inventoryNumber;
-        //        }
-
-        //    }
-        //    while (availablePassportNumbers.Contains(inventoryNumber));
-        //    return 0;
-        //}
         public int Length
         {
             get; private set;
@@ -92,7 +68,6 @@ namespace HomWorkClasses25._10._2020_1
             GenerateInventoryNumber(availableLandPlots);
             GenerateLengthWight();
             GenerateResidentsCount();
-
         }
         public void AddGeneratedLandPlotWithParametrs(List<LandPlot> availableLandPlots)
         {
